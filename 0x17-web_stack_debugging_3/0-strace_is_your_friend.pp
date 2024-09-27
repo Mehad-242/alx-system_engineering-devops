4658c68ea0df1e06e5b862cac990f30dd03c90df
Mehad-242 <mehadboosh@gmail.com>
2024-09-27 01:00:25 +0000

    web_stack_debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# creat a manifest that fix all termintion of phpp.
-
-exec { 'fix_phpp':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+# Manifest to fix apache configuration
+exec { 'fix config file':
+	command =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+	path    => '/bin'
 }
