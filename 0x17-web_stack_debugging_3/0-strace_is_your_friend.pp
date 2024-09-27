ae593e2b31772d40b0009586852f5115b12e5588
Mehad-242 <mehadboosh@gmail.com>
2024-09-27 07:33:37 +0000

    strace_is_your_friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,5 @@
-# Puppet script to fix Apache 500 error 
-
-file { '/var/www/html/index.php':
-   ensure => file,
-   owner   => 'www-data',
-   group   => 'www-data',
-   mode     => '0644',
-} 
-
-service { 'apache2':
-   ensure         => running,
-   enable         => true,
-   hasrestart => true,
-   subscribe   => File['/var/www/html/index.php'],
-} 
-
-exec { 'fix-wordpress':
-   command => '/usr/bin/chown -R www-data:www-data /var/www/html',
-   onlyif   => 'test $(stat -c %U /var/www/html/index.php) != "www-data"',
-   notify   => Service['apache2'],
+# Manifest to fix apache configuration
+exec { 'fix config file':
+	command =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+	path    => '/bin'
 }
