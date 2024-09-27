afbf7baf9bcc20199fce2a127b935e4a045d1e24
Mehad-242 <mehadboosh@gmail.com>
2024-09-27 01:18:50 +0000

    web_stack_debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,13 @@
-exec { 'change-os-configuration-for-holberton-user':
-	command => 'echo "holberton soft nofile 4096" >> /etc/security/limits.conf && echo "holberton hard nofile 4096" >> /etc/security/limits.conf',
-	path    => ['/bin', '/usr/bin'],
-	unless  => 'grep -q "holberton" /etc/security/limits.conf',
+# Enable the user holberton to login and open files without error.
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+	command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+	path => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+	command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+	path    => '/usr/local/bin/:/bin/'
 }
